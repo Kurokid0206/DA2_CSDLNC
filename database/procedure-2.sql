0f5f08dc246f64c3be7c522b6ae739444a4c04ed
Mean-Sun <tranminhsonyi@gmail.com>
2021-12-31 16:49:21 +0700

    update UI

--- database/procedure-2.sql
+++ database/procedure-2.sql
@@ -1,2 +1,37 @@
 use QLBanHoa
 go
+
+--DROP PROCEDURE sp_Insert_SanPham
+CREATE PROCEDURE sp_Insert_SanPham
+    @MaSP char(10) output,
+    @TenSP nvarchar(50),
+    @SoLuongTon int,
+    @GiaBan int,
+    @MauSac nvarchar(50),
+    @ChuDe nvarchar(50)
+
+AS
+
+BEGIN TRAN
+    BEGIN TRY
+        SET @MaSP = dbo.f_Auto_MaSP()
+        INSERT INTO SanPham
+        values(@MaSP, @TenSP, @SoLuongTon, NULL, @MauSac, @ChuDe)
+		declare @NgayApDung as date
+        SET @NgayApDung = GETDATE()
+        INSERT INTO BangGiaSP
+        values(@MaSP, @NgayApDung, NULL, @GiaBan)
+    end try
+    begin catch
+        select  error_number() as errornumber,
+                error_severity() as errorseverity, 
+                error_state() as errorstate,
+                error_procedure() as errorprocedure,
+                error_line() as errorline,
+                error_message() as errormessage; 
+        if @@trancount > 0
+            rollback tran
+    end catch
+if @@trancount > 0
+    commit tran;
+GO
--- publics/js/manager-products-function.js
+++ publics/js/manager-products-function.js
@@ -16,6 +16,93 @@ function section_show(id) {
     div.style.display = "block";
 }
 
+function get_product_for_manage() {
+    var name = document.getElementById("product_name-for-seach-manager-products").value;
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        render_manager_view_products(JSON.parse(this.responseText))
+            // input.value="";
+            // var data=JSON.parse(this.responseText)
+        console.log(this.responseText)
+
+    }
+
+    xhtml.open("POST", "/manage_product/view-products");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send('nameSearch=' + name);
+
+}
+
+function render_manager_view_products(data) {
+    var table = document.querySelector("#manage-products-table tbody");
+    if (data.length <= 0) {
+        table.innerHTML = "No result."
+        return
+    }
+    var tr = ``
+    data.forEach(element => {
+        tr = tr + `<tr id="${element.MaSP}">
+        <td>
+            <h6 style="margin:15px 0 0 0;">${element.MaSP}</h6>
+        </td>
+        <td>
+            <input class="TenSP-for-edit" type="text" value="${element.TenSP}" disabled>
+        </td>
+        <td>
+            <input class="MauSac-for-edit" type="text" value="${element.MauSac}" disabled>
+        </td>
+        <td>
+            <input class="ChuDe-for-edit" type="text" value="${element.ChuDe}" disabled>
+        </td>
+        <td>
+            <input class="GiaBan-for-edit" type="number" value="${element.GiaBan}" disabled>
+        </td>
+        <td>
+            <h6 style="margin:15px 0 0 0;">${element.SoLuongTon}</h6>
+        </td>
+        <td style="padding-top:15px;">
+            <button type="button" class="btn-primary save-btn" onclick="save_manage_edit_product('${element.MaSP}');"  disabled style="display:none">Save</button>
+            <button type="button" class="btn-secondary" onclick="enable_edit('${element.MaSP}')">Edit</button>
+        </td>
+
+    </tr>`
+    });
+    table.innerHTML = tr
+}
+
+function save_manage_edit_product(MaSP) {
+    var tr = document.getElementById(MaSP);
+    var TenSP = tr.querySelector(` .TenSP-for-edit`).value;
+    var MauSac = tr.querySelector(` .MauSac-for-edit`).value;
+    var ChuDe = tr.querySelector(` .ChuDe-for-edit`).value;
+    var GiaBan = tr.querySelector(` .GiaBan-for-edit`).value;
+    var tr = document.getElementById(MaSP);
+    tr.querySelector(` .TenSP-for-edit`).disabled = true;
+    tr.querySelector(` .MauSac-for-edit`).disabled = true;
+    tr.querySelector(` .ChuDe-for-edit`).disabled = true;
+    tr.querySelector(` .GiaBan-for-edit`).disabled = true;
+    tr.querySelector(` .save-btn`).style.display = "none";
+
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        console.log(this.responseText)
+
+    }
+
+    xhtml.open("POST", "/manage_product/save-edit-products");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send(`MaSP=${MaSP}&TenSP=${TenSP}&MauSac=${MauSac}&ChuDe=${ChuDe}&GiaBan=${GiaBan}`);
+}
+
+function enable_edit(MaSP) {
+    var tr = document.getElementById(MaSP);
+    tr.querySelector(` .TenSP-for-edit`).disabled = false;
+    tr.querySelector(` .MauSac-for-edit`).disabled = false;
+    tr.querySelector(` .ChuDe-for-edit`).disabled = false;
+    tr.querySelector(` .GiaBan-for-edit`).disabled = false;
+    tr.querySelector(` .save-btn`).disabled = false;
+    tr.querySelector(` .save-btn`).style.display = "inline-block"
+}
 
 function add_order_detail() {
     var container = document.querySelector(`#order_detail-container`);
@@ -52,4 +139,30 @@ function delete_componentbyID(CTDH) {
 function show_form(id) {
     var div = document.querySelector("#" + id);
     div.style.display = "block";
+}
+
+function manage_add_new_product() {
+    var form = document.getElementById("add-new-product-form")
+    var TenSP = form[0].value;
+    var MauSac = form[1].value;
+    var ChuDe = form[2].value;
+    var GiaBan = form[3].value;
+    var SLTon = form[4].value;
+
+
+
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+
+        // input.value="";
+        // var data=JSON.parse(this.responseText)
+        console.log(this.responseText)
+        section_show('manager-products-section');
+        get_product_for_manage();
+
+    }
+
+    xhtml.open("POST", "/manage_product/insert-product");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send(`TenSP=${TenSP}&MauSac=${MauSac}&ChuDe=${ChuDe}&GiaBan=${GiaBan}&SLTon=${SLTon}`);
 }
