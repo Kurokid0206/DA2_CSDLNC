fe16b210ae15606a974658d8728f3520d0ac471d
Mean-Sun <tranminhsonyi@gmail.com>
2022-01-01 14:24:15 +0700

    update manage-products UI

--- publics/js/manager-products-function.js
+++ publics/js/manager-products-function.js
@@ -23,7 +23,7 @@ function get_product_for_manage() {
         render_manager_view_products(JSON.parse(this.responseText))
             // input.value="";
             // var data=JSON.parse(this.responseText)
-        console.log(this.responseText)
+            //console.log(this.responseText)
 
     }
 
@@ -104,33 +104,7 @@ function enable_edit(MaSP) {
     tr.querySelector(` .save-btn`).style.display = "inline-block"
 }
 
-function add_order_detail() {
-    var container = document.querySelector(`#order_detail-container`);
-    var CTDHs = document.querySelectorAll('.CT_donhang');
 
-    var temp = document.createElement('div');
-    temp.setAttribute('id', `CTDH${CTDHs.length}`)
-    temp.classList.add('CT_donhang')
-    temp.classList.add('enter-box')
-    temp.innerHTML = `
-    <h4 style="margin:5px 0 0 0; display: inline-block;">Sản phẩm ${CTDHs.length+1}:</h4>
-    <button class="btn-danger" type="button" onclick="delete_componentbyID('CTDH${CTDHs.length}')" style="display: inline-block; margin-left: 50%;">
-        <h6 style=" margin:5px 0 0 0; color: aliceblue; ">Xóa</h6>
-    </button>
-    <br><br>
-
-    <h6 style="margin:5px 0 0 0;">Sản phẩm:</h6>
-    <select name="product" id="select-product" placeholder="Chọn đối tác" required>
-        <option selected>Chọn sản phẩm</option>
-        <option value="Mã sp">Tên sản phẩm</option>
-    </select>
-    <h6 style="margin:5px 0 0 0;">Số lượng:</h6>
-    <input type="number" name="quantity" placeholder="Số lượng" required/>
-    <br><br>`
-    container.appendChild(temp);
-    get_product(`CTDH${CTDHs.length}`)
-
-}
 
 function delete_componentbyID(CTDH) {
     document.getElementById(CTDH).remove();
@@ -156,7 +130,8 @@ function manage_add_new_product() {
 
         // input.value="";
         // var data=JSON.parse(this.responseText)
-        console.log(this.responseText)
+        //console.log(this.responseText)
+        document.getElementById("add-product-container").style.display = "none"
         section_show('manager-products-section');
         get_product_for_manage();
 
@@ -165,4 +140,242 @@ function manage_add_new_product() {
     xhtml.open("POST", "/manage_product/insert-product");
     xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhtml.send(`TenSP=${TenSP}&MauSac=${MauSac}&ChuDe=${ChuDe}&GiaBan=${GiaBan}&SLTon=${SLTon}`);
+    return false;
+}
+
+
+
+function get_products_price() {
+    var name = document.getElementById("product_ID-for-seach-manager-products-price").value;
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        render_manager_view_products_price(JSON.parse(this.responseText))
+            // input.value="";
+            // var data=JSON.parse(this.responseText)
+            //console.log(this.responseText)
+
+    }
+
+    xhtml.open("POST", "/manage_product/view-products-price");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send('MaSP=' + name);
+
+}
+
+
+function render_manager_view_products_price(data) {
+    var table = document.querySelector("#manager-products-price-table tbody");
+    if (data.length <= 0) {
+        table.innerHTML = "No result."
+        return
+    }
+    var tr = ``
+    data.forEach(element => {
+        tr = tr + `<tr>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${element.MaSP}</h6>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${element.TenSP}</h>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${element.GiaNhap}</h>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${element.GiaBan}</h>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${new Date(element.NgayApDung).toISOString().slice(0, 10)}</h>
+        </td>
+
+    </tr>`
+    });
+    table.innerHTML = tr
+}
+
+
+function get_warehouse_data() {
+    var name = document.getElementById("product_name-for-seach-warehouse").value;
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        render_warehouse_data(JSON.parse(this.responseText))
+            // input.value="";
+            // var data=JSON.parse(this.responseText)
+            //console.log(this.responseText)
+
+    }
+
+    xhtml.open("POST", "/manage_product/view-warehouse-data");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send('nameSearch=' + name);
+
+}
+
+function render_warehouse_data(data) {
+    var table = document.querySelector("#manager-warehouse-table tbody");
+    if (data.length <= 0) {
+        table.innerHTML = "No result."
+        return
+    }
+    var tr = ``
+    data.forEach(element => {
+        tr = tr + `<tr>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${element.MaSP}</h6>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">${element.TenSP}</h>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;color: rgb(0, 161, 27)">${element.SoLuongTon}</h>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;color: rgb(255, 0, 119);">100</h>
+        </td>
+        <td>
+            <button type="button" class="btn-primary" onclick="show_import_goods_form('${element.MaSP}');">Nhập hàng</button>
+        </td>
+    </tr>`
+    });
+    table.innerHTML = tr
+}
+
+
+function show_import_goods_form(MaSP) {
+    section_show('new-import-goods-section');
+    get_product_forAll();
+    // document.querySelector("#new-import-goods-section #CTDH0 option").value = MaSP;
+    // document.querySelector("#new-import-goods-section #CTDH0 option").innerHTML = MaSP;
+    // document.querySelector("#new-import-goods-section #CTDH0").disabled = true;
+
+}
+
+function add_order_detail() {
+    var container = document.querySelector(`#order_detail-container`);
+    var CTDHs = document.querySelectorAll('.CT_donhang');
+
+    var temp = document.createElement('div');
+    temp.setAttribute('id', `CTDH${CTDHs.length}`)
+    temp.classList.add('CT_donhang')
+    temp.classList.add('enter-box')
+    temp.innerHTML = `
+    <h4 style="margin:5px 0 0 0; display: inline-block;">Sản phẩm ${CTDHs.length+1}:</h4>
+    <button class="btn-danger" type="button" onclick="delete_componentbyID('CTDH${CTDHs.length}')" style="display: inline-block; margin-left: 50%;">
+        <h6 style=" margin:5px 0 0 0; color: aliceblue; ">Xóa</h6>
+    </button>
+    <br><br>
+
+    <h6 style="margin:5px 0 0 0;">Sản phẩm:</h6>
+    <select name="product" id="select-product" placeholder="Chọn đối tác" required>
+        <option selected>Chọn sản phẩm</option>
+        <option value="Mã sp">Tên sản phẩm</option>
+    </select>
+    <h6 style="margin:5px 0 0 0;">Số lượng:</h6>
+    <input type="number" name="quantity" placeholder="Số lượng" required/>
+    <br><br>`
+    container.appendChild(temp);
+    get_product_forOne(`CTDH${CTDHs.length}`)
+
+}
+
+function get_product_forAll() {
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        var CTDHs = document.querySelectorAll(".CT_donhang")
+        CTDHs.forEach(element => {
+            render_product_for_import(JSON.parse(this.responseText), element)
+        })
+
+    }
+    xhtml.open("GET", "/manage_product/product-data");
+    //xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send();
+}
+
+function get_product_forOne(ID) {
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        var temp = document.getElementById(ID)
+
+        render_product_for_import(JSON.parse(this.responseText), temp)
+    }
+
+
+    xhtml.open("GET", "/manage_product/product-data");
+    //xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send();
+}
+
+function render_product_for_import(data, target) {
+    var product = target.querySelector(`#select-product`)
+    var opt = `<option selected>Chọn Sản Phẩm</option>`
+    data.forEach(element => {
+        opt = opt + `<option value="${element.MaSP}">${element.TenSP} - ${element.MaSP}</option>`
+    });
+    product.innerHTML = opt
+}
+
+function import_goods() {
+    var products = document.querySelectorAll('.CT_donhang');
+    var data = [];
+    products.forEach(product => {
+        let temp = { MaSP: "", SoLuong: 0 };
+        temp.MaSP = product.querySelector('#select-product').value
+        temp.SoLuong = parseInt(product.querySelector('input').value)
+        data.push(temp);
+        temp = { MaSP: "", SoLuong: 0 };
+    })
+    data = JSON.stringify(data);
+    let date = document.getElementById("NgayNhap").value
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+        customer_view_order();
+    }
+
+    xhtml.open("POST", "/manage_product/import-goods");
+    xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send(`date=${date}&data=${data}`);
+
+    return false;
+}
+
+function get_import_history() {
+    var xhtml = new XMLHttpRequest();
+    xhtml.onload = function() {
+
+        render_import_history(JSON.parse(this.responseText))
+    }
+
+
+    xhtml.open("GET", "/manage_product/import-history-data");
+    //xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhtml.send();
+}
+
+function render_import_history(data) {
+    var table = document.querySelector("#manager-import-history-table tbody");
+    if (data.length <= 0) {
+        table.innerHTML = "No result."
+        return
+    }
+    var tr = ``
+    data.forEach(element => {
+        tr = tr + `<tr>
+        <td>
+            <h6 style="margin:5px 0 0 0;">$${element.MaDN}</h6>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">$${element.NgayNhap}</h>
+        </td>
+        <td>
+            <h6 style="margin:5px 0 0 0;">$${element.TongTien}</h>
+        </td>
+        <td>
+            <button type="button" class="btn-primary" onclick="get_product();">
+                <h6 style="margin:5px 0 0 0;color: cornsilk;">Chi tiết</h6>
+            </button>
+        </td>
+    </tr>`
+    });
+    table.innerHTML = tr
 }
