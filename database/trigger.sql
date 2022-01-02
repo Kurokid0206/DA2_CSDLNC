c82ae878cf3e4e9c77642641f8273ad8f06f255d
YuukiTS01 <93564033+YuukiTS01@users.noreply.github.com>
2022-01-02 11:13:09 +0700

    update proc, trigger and data gen

--- database/trigger.sql
+++ database/trigger.sql
@@ -16,3 +16,60 @@ begin
 		set HieuSuat = @HieuSuat where  MaNV = @MaNV
 	end
 end
+go
+
+create trigger trg_ThanhTien
+on CT_HoaDon
+for insert, update
+as
+begin
+	if UPDATE(SoLuong)
+	begin
+		declare @MaSP as char(10) = (select MaSP from inserted)
+		declare @MaHD as char(10) = (select MaHD from inserted)
+		declare @GiaBan as int = (select GiaBan from BangGiaSP 
+								where MaSP = @MaSP and NgayApDung = (select max(NgayApDung) from BangGiaSP 
+																	where MaSP = @MaSP))
+		update CT_HoaDon
+		set ThanhTien = SoLuong * @GiaBan
+		where MaSP = @MaSP and MaHD = @MaHD
+	end
+end
+go
+
+create trigger trg_TongTien
+on CT_HoaDon
+after insert, update, delete
+as
+begin
+	declare @MaHD as char(10) = NULL
+	if UPDATE(ThanhTien) or UPDATE(MaHD)
+		set @MaHD = (select MaHD from inserted)
+	else 
+		set @MaHD = (select MaHD from deleted)
+
+	declare @MaGiamGia as varchar(10) = (select MaGiamGia from HoaDon where MaHD = @MaHD)
+	declare @GiaGiam as int = (select SoTienGiam from GiamGia where MaGiamGia = @MaGiamGia)
+							+ (select PhanTramGiam from GiamGia where MaGiamGia = @MaGiamGia)/100
+	update HoaDon
+	set TongTien = (select (SUM(ThanhTien) - @GiaGiam) from CT_HoaDon where MaHD = @MaHD) 
+	where MaHD = @MaHD
+end
+go
+
+create trigger trg_TenND
+on TaiKhoan
+after insert, update
+as
+begin
+	if UPDATE(NguoiDung)
+		declare @TK as varchar(50) = (select TenTK from inserted)
+		declare @VaiTro as nvarchar(50) = (select VaiTro from inserted)
+		declare @Ten as nvarchar(50) = (select NguoiDung from inserted)
+	if @VaiTro like N'Khách Hàng'
+		update KhachHang
+		set HoTen = @Ten
+	if @VaiTro like N'Nhân Viên'
+		update NhanVien
+		set TenNV = @Ten
+end
