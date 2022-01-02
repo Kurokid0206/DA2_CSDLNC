c15d743c85968a836b0c9cda4ad4fb279aa08d7f
Mean-Sun <tranminhsonyi@gmail.com>
2022-01-02 13:30:37 +0700

    update small

--- database/procedure-2.sql
+++ database/procedure-2.sql
@@ -2,39 +2,28 @@
 go
 
 --DROP PROCEDURE sp_Insert_SanPham
-CREATE PROCEDURE sp_Insert_SanPham
-    @MaSP char(10) output,
+create procedure sp_Insert_SanPham
+	@MaSP char(10) output,
     @TenSP nvarchar(50),
-    @SoLuongTon int,
-    @GiaBan int,
-    @MauSac nvarchar(50),
-    @ChuDe nvarchar(50)
-
-AS
-
-BEGIN TRAN
-    BEGIN TRY
-        SET @MaSP = dbo.f_Auto_MaSP()
-        INSERT INTO SanPham
-        values(@MaSP, @TenSP, @SoLuongTon, NULL, @MauSac, @ChuDe)
-		declare @NgayApDung as date
-        SET @NgayApDung = GETDATE()
-        INSERT INTO BangGiaSP
-        values(@MaSP, @NgayApDung, NULL, @GiaBan)
-    end try
-    begin catch
-        select  error_number() as errornumber,
-                error_severity() as errorseverity, 
-                error_state() as errorstate,
-                error_procedure() as errorprocedure,
-                error_line() as errorline,
-                error_message() as errormessage; 
-        if @@trancount > 0
-            rollback tran
-    end catch
-if @@trancount > 0
+    @SL int,
+	@LoaiSP nvarchar(50),
+	@Mau nvarchar(50),
+	@ChuDe nvarchar(100)
+as
+begin tran
+	begin try
+		set @MaSP = dbo.f_Auto_MaSP()
+		insert into SanPham
+		values(@MaSP, @TenSP, @SL, @LoaiSP, @Mau, @ChuDe)
+	end try
+	begin catch
+		select  error_message() as errormessage; 
+		if @@trancount > 0  
+			rollback tran
+	end catch
+if @@trancount > 0  
     commit tran;
-GO
+go
 
 --DROP PROCEDURE sp_View_DonNhapHang
 CREATE PROCEDURE sp_View_DonNhapHang
