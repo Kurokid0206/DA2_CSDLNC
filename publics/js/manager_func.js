3e14fcd562c49185b907342368ca42dbda40242a
Kurokid0206 <hungtyvuive@gmail.com>
2022-01-02 15:31:54 +0700

    full emp manager + update manager functions

--- publics/js/manager_func.js
+++ publics/js/manager_func.js
@@ -6,8 +6,11 @@ var ids = [
   'Revenue-compare-section',
 ]
 var glb_data = []
+const page_max = 10
+var called=false
 
 function show(id) {
+  glb_data = []
   ids.forEach((id) => {
     var div = document.querySelector('#' + id)
     div.style.display = 'none'
@@ -15,12 +18,15 @@ function show(id) {
   var div = document.querySelector('#' + id)
   div.style.display = 'flex'
 }
+
 function get_revenue() {
   let month = document.querySelector('#Revenue-section select').value
   var xhtml = new XMLHttpRequest()
   xhtml.onload = function () {
     glb_data = JSON.parse(this.responseText)
-    render_revenue(glb_data)
+    document.querySelector('#Revenue-section .page-number').value = 1
+    render_revenue()
+    if(!called){get_total_rev() }
   }
 
   xhtml.open('get', 'manager/get-revenue')
@@ -28,24 +34,23 @@ function get_revenue() {
   xhtml.send(`month=${month}`)
 }
 
-function render_revenue(data) {
-  show('Revenue-section');
+function render_revenue() {
+  let data = glb_data
   let page = document.querySelector('#Revenue-section .page-number').value
   tr = ``
-  for (var i = (page - 1) * 2; i < page * 2; i++) {//need replace
-
-    // if(i>=data.length){break;}
+  for (var i = (page - 1) * page_max; i < page * page_max; i++) {
     try {
+      let element = data[i]
       tr += `
     <tr>
       <td scope="col" style="width: 100px;">
-          <h6 style="margin:5px 0 0 0;">${data[i].MaSP}</h6>
+          <h6 style="margin:5px 0 0 0;">${element.MaSP}</h6>
       </td>
       <td scope="col" style="width: 300px;">
-          <h6 style="margin:5px 0 0 0;">${data[i].TenSP}</h6>
+          <h6 style="margin:5px 0 0 0;">${element.TenSP}</h6>
       </td>
       <td scope="col" style="width: 200px;">
-          <h6 style="margin:5px 0 0 0;">${data[i].DoanhThu}</h6>
+          <h6 style="margin:5px 0 0 0;">${element.DoanhThu}</h6>
       </td>
   </tr>`
     } catch (err) {
@@ -54,34 +59,79 @@ function render_revenue(data) {
   }
   document.querySelector('#Revenue-section tbody').innerHTML = tr
 }
-
+function get_total_rev(){
+  let data=glb_data
+  sum=0
+  data.forEach(element=>{
+    sum+=element.DoanhThu
+  })
+  document.getElementById("Sum_Revenue").innerHTML=sum
+}
 function next(name) {
   let page = document.querySelector(`#${name} .page-number`)
-  //console.log(glb_data.length/100)
-  if(page.value<glb_data.length/2){//(glb_data.length/100)){
+  if (page.value < glb_data.length / page_max) {
     page.value++
-    render_revenue(glb_data)
   }
-  
 }
-function prev(name) {
 
+function prev(name) {
   let page = document.querySelector(`#${name} .page-number`)
-  if(page.value>=2){
+  if (page.value >= 2) {
     page.value--
-    render_revenue(glb_data)
   }
-  
 }
+
+function get_eff() {
+  let month = document.querySelector('#emp-eff-section select').value
+  var xhtml = new XMLHttpRequest()
+  xhtml.onload = function () {
+    document.querySelector('#emp-eff-section .page-number').value=1
+    glb_data = JSON.parse(this.responseText)
+    render_eff()
+
+  }
+
+  xhtml.open('post', 'manager/get-eff')
+  xhtml.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+  xhtml.send(`month=${month}`)
+}
+
+function render_eff() {
+   let data = glb_data
+
+  if(data.length<1){
+    document.querySelector('#emp-eff-section tbody').innerHTML =`
+    <tr><td scope="col" >
+    <h6 style="margin:5px 0 0 0;">No result</h6></td>`
+    return
+  }
+  let page = document.querySelector('#emp-eff-section .page-number').value
+  tr = ``
+  for (var i = (page - 1) * page_max; i < page * page_max; i++) {
+    try {
+      let emp = data[i]
+      tr += `<tr><td scope="col" >
+    <h6 style="margin:5px 0 0 0;">${emp.MaNV}</h6></td>
+    <td scope="col" >
+    <h6 style="margin:5px 0 0 0;">${emp.TenNV}</h6></td>
+    <td scope="col" >
+    <h6 style="margin:5px 0 0 0;">${emp.MucTieu}</h6></td>
+    <td scope="col">
+    <h6 style="margin:5px 0 0 0;">${emp.HieuSuat}</h6></td></tr>`
+    } catch (err) {
+      break
+    }
+  }
+  document.querySelector('#emp-eff-section tbody').innerHTML = tr
+}
+
 function get_discount() {
   show('discount-section')
   var xhtml = new XMLHttpRequest()
   xhtml.onload = function () {
-    let data = JSON.parse(this.responseText)
-    test = data
-    document.querySelector('#discount-table tbody').innerHTML = render_discount(
-      data,
-    )
+    glb_data = JSON.parse(this.responseText)
+    document.querySelector('#discount-section .page-number').value = 1
+    render_discount()
     document.getElementById('btn-add-discount').style.display = 'inline-block'
   }
 
@@ -89,34 +139,43 @@ function get_discount() {
   //xhtml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhtml.send()
 }
-function render_discount(data) {
+
+function render_discount() {
+  let data = glb_data
+  let page = document.querySelector('#discount-section .page-number').value
   if (data.length < 1) {
     return `<tr><td scope="col">
         <h6 style="margin:5px 0 0 0;">No result</h6>
         </td><tr>`
   }
   tr = ``
-  data.forEach((discount) => {
-    tr += `
+  for (var i = (page - 1) * page_max; i < page * page_max; i++) {
+    let discount = data[i]
+    try {
+      tr += `
         <tr><td scope="col">
         <h6 style="margin:5px 0 0 0;">${discount.MaGiamGia}</h6></td>`
-    if (discount.LoaiGiamGia == '0') {
-      tr += `<td scope="col">
+      if (discount.LoaiGiamGia) {
+        tr += `<td scope="col">
+            <h6 style="margin:5px 0 0 0;">${discount.PhanTramGiam}%</h6></td>`
+      } else {
+        tr += `<td scope="col">
             <h6 style="margin:5px 0 0 0;">${discount.SoTienGiam.toFixed(
               0,
             ).replace(/\d(?=(\d{3})+\.)/g, '$&,')} VND</h6></td>`
-    } else {
-      tr += `<td scope="col">
-            <h6 style="margin:5px 0 0 0;">${discount.PhanTramGiam}%</h6></td>`
-    }
+      }
 
-    tr += `<td scope="col" >
+      tr += `<td scope="col" >
         <h6 style="margin:5px 0 0 0;">${new Date(discount.NgayHetHan)
           .toISOString()
           .slice(0, 10)}</h6></td></tr>`
-  })
-  return tr
+    } catch (err) {
+      break
+    }
+  }
+  document.querySelector('#discount-table tbody').innerHTML = tr
 }
+
 function show_add_discount() {
   document.querySelector('#discount-section .error-message').style.display =
     'none'
--- routes/manager.js
+++ routes/manager.js
@@ -18,12 +18,49 @@ config = {
 router.get('/', function (req, res) {
   res.render('manager')
 })
-data = [{ MaSP: 'SP001', TenSP: 'Name001', DoanhThu: 10000, DoanhThu2: 15000 },
-{ MaSP: 'SP002', TenSP: 'Name002', DoanhThu: 20000, DoanhThu2: 25000 },
-{ MaSP: 'SP003', TenSP: 'Name003', DoanhThu: 30000, DoanhThu2: 35000 },
+
+data = [
+  { MaSP: 'SP001', TenSP: 'Name001', DoanhThu: 10000, DoanhThu2: 15000 },
+  { MaSP: 'SP002', TenSP: 'Name002', DoanhThu: 20000, DoanhThu2: 25000 },
+  { MaSP: 'SP003', TenSP: 'Name003', DoanhThu: 30000, DoanhThu2: 35000 },
+  { MaSP: 'SP004', TenSP: 'Name004', DoanhThu: 40000, DoanhThu2: 45000 },
 ]
+
 router.get('/get-revenue', (req, res) => {
-    res.send(data)
+  res.send(data)
+  // (async () => {
+  //   try {
+  //     let pool = await sql.connect(config)
+  //     let result = await pool.request()
+
+  //         .execute('sp_ThemMaGiam')
+  //     pool.close()
+  //     res.send(result.recordset)
+  //     //console.log(result)
+  //   } catch (error) {
+  //     //console.log(error.message);
+  //     res.send(error.message)
+  //     return error
+  //   }
+  // })()
+})
+
+router.post('/get-eff', (req, res) => {
+  ;(async () => {
+    try {
+      let pool = await sql.connect(config)
+      let result = await pool.request()
+        .input('thang',sql.Int,req.body.month)
+        .execute('sp_Get_HieuSuat')
+      pool.close()
+      res.send(result.recordset)
+      //console.log(req.body)
+      //console.log(result)
+    } catch (error) {
+      console.log(error.message)
+      return error
+    }
+  })()
 })
 
 router.get('/get-discount', (req, res) => {
@@ -44,31 +81,26 @@ router.post('/add-discount', (req, res) => {
   let data = req.body
   discount = data.discount
   number = 0
-  type = 0
+  type = false
   if (discount.indexOf('%') > 0) {
-    percent = parseInt(data.discount.toString().slice(0, discount.indexOf('%')))
-    number = null
-    type = 1
+    number = parseInt(data.discount.toString().slice(0, discount.indexOf('%')))
+    type = true
   } else {
-    percent = null
     number = parseInt(data.discount)
-    type = 0
+    
   }
 
   ;(async () => {
     try {
       let pool = await sql.connect(config)
-      let result = await pool.query(`
-                insert into GiamGia values(
-                    '${data.code}','${type}',${number},${percent},'${data.date}')
-            `)
-      // .request()
-      //     .input('MaGiamGia',sql.VarChar(10),data.code)
-      //     .input('LoaiGiam',sql.VarChar(10),data.code)
-      //     .input('SoTienGiam',sql.Int,number)
-      //     .input('PhanTramGiam',sql.Int,percent)
-      //     .input('NgayHetHan',sql.VarChar(10),data.date)
-      //     .execute('sp_ThemMaGiam')
+      let result = await pool
+        .request()
+        .input('MaGiamGia', sql.VarChar(10), data.code)
+        .input('LoaiGiamGia', sql.Bit, type)
+        .input('GiamGia', sql.Int, number)
+        .input('NgayHetHan', sql.VarChar(10), data.date)
+        .execute('sp_Insert_GiamGia')
+
       pool.close()
       res.send(result.recordset)
       //console.log(result)
