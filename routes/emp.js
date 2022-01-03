cd2ba6d22390fb39d1481ab79634bef6f793efd5
Mean-Sun <tranminhsonyi@gmail.com>
2022-01-03 15:15:27 +0700

    update API epm

--- routes/emp.js
+++ routes/emp.js
@@ -43,6 +43,92 @@ router.get("/get-order", function(req, res) {
 })
 
 
+//driver receive order
+router.post("/recv-order", function(req, res) {
+    (async() => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                .input('MaNV', sql.Char(10), req.session.user) //Thay bằng session
+                .input('MaHD', sql.Char(10), req.body.MaHD)
+                .execute('sp_NV_NhanDon')
+
+
+            pool.close()
+                //console.log(result)
+            res.send(result.recordset)
+        } catch (err) {
+            console.log(err.message)
+            res.send(err.message)
+        }
+
+    })()
+})
+
+
+//driver view all received order
+router.post("/get-received-order", function(req, res) {
+    (async() => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                .query(`Select * from HoaDon where MaNV = '${req.session.user}' and TrangThai = N'Đang Giao'`) //thay bằng session
+
+
+            pool.close()
+                //console.log(result)
+            res.send(result.recordset)
+        } catch (err) {
+            console.log(err.message)
+            res.send(err.message)
+        }
+
+    })()
+})
+
+
+//driver view all order have been comfirmed
+router.post("/get-comfirmed-order", function(req, res) {
+    (async() => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                .query(`Select * from HoaDon where MaNV = '${req.session.user}' and TrangThai = N'Đã Giao'`) //thay bằng session
+
+
+            pool.close()
+                //console.log(result)
+            res.send(result.recordset)
+        } catch (err) {
+            console.log(err.message)
+            res.send(err.message)
+        }
+
+    })()
+})
+
+
+
+//driver view salary
+router.get("/get-salary", function(req, res) {
+    (async() => {
+        try {
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                .query(`select * from BangLuong where MaNV='${req.session.user}'`)
+
+
+            pool.close()
+                //console.log(result.recordset[0])
+            res.send(result.recordset)
+        } catch (err) {
+            console.log(err.message)
+            res.send(err.message)
+        }
+
+    })()
+})
+
 
 
 module.exports = router
